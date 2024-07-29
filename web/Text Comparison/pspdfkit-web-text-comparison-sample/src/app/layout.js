2b90212e12cab9731f717ba75bd32933313f76c7
Jon Addams <jonaddams@gmail.com>
2024-07-29 15:05:21 -0400

    Add text comparison sample

---
+++ web/Text Comparison/pspdfkit-web-text-comparison-sample/src/app/layout.js
@@ -0,0 +1,14 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "PSPDFKit Web SDK Text Comparison Example",
+  description: "PSPDFKit Web SDK Text Comparison Example",
+};
+
+export default function RootLayout({children}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
